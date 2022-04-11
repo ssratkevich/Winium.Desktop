0aa907c396b7334381cf0385ccf533a8f2ff2f86
Sergey Ratkevich <ratkevich@syntellect.ru>
2022-04-11 12:59:40 +0300

    Extended sendKeys support.
    Extended support of SendKeys command when keys sended as a single string, not as array of chars.

--- src/Winium.Desktop.Driver/CommandExecutors/SendKeysToActiveElementExecutor.cs
+++ src/Winium.Desktop.Driver/CommandExecutors/SendKeysToActiveElementExecutor.cs
@@ -3,6 +3,7 @@
     #region using
 
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     #endregion
@@ -13,7 +14,24 @@
 
         protected override string DoImpl()
         {
-            var chars = this.ExecutedCommand.Parameters["value"].Select(x => Convert.ToChar(x.ToString()));
+            var chars = new List<char>();
+            foreach (var token in this.ExecutedCommand.Parameters["value"])
+            {
+                var str = token.ToString();
+                if (string.IsNullOrEmpty(str))
+                {
+                    continue;
+                }
+                if (str.Length == 1)
+                {
+                    chars.Add(Convert.ToChar(str));
+                }
+                else
+                {
+                    chars.AddRange(str.ToCharArray());
+                }
+            }
+            //var chars = this.ExecutedCommand.Parameters["value"].Select(x => Convert.ToChar(x.ToString()));
 
             this.Automator.WiniumKeyboard.SendKeys(chars.ToArray());
 
